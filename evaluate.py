54d9d91c902ac87e3aea83cc768e8552dd8e63b1
ykawabata17 <AI1063@nara.kosen-ac.jp>
2021-12-16 16:49:46 +0900

    sample Add

---
+++ evaluate.py
@@ -0,0 +1,27 @@
+from keras.models import load_model
+from tensorflow.keras.utils import to_categorical
+import glob
+import cv2
+import numpy as np
+import re
+
+model = load_model('simple_model.h5')
+files = glob.glob("eps_0.10/*.jpg")
+
+test_img = []
+test_label = []
+
+for file in files:
+    img = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+    test_img.append(img)
+    num = re.sub(r"\D", "", file)
+    test_label.append(num[3])
+
+test_img = np.array(test_img).astype('float32') / 255
+test_img = test_img.reshape(len(test_img),28,28,1)
+test_label = to_categorical(test_label)
+
+test_loss, test_acc = model.evaluate(test_img, test_label, verbose=0)
+
+print(test_acc)
+print(test_loss)
--- fgsm_adversarial.py
+++ fgsm_adversarial.py
@@ -4,22 +4,14 @@ from pyimagesearch.fgsm import generate_image_adversary
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.datasets import mnist
+from keras.models import load_model
 import numpy as np
 import cv2
 
 # load MNIST dataset and scale the pixel values to the range [0, 1]
 print("[INFO] loading MNIST dataset...")
 (trainX, trainY), (testX, testY) = mnist.load_data()
-for i in range(trainX.shape[0]):
-    th_train = trainX[i]
-    th_train[trainX[i]<100] = 0
-    th_train[trainX[i]>=100] = 255
-    trainX[i] = th_train
-for i in range(testX.shape[0]):
-    th_train = testX[i]
-    th_train[testX[i]<100] = 0
-    th_train[testX[i]>=100] = 255
-    testX[i] = th_train
+
 trainX = trainX / 255.0
 testX = testX / 255.0
 
@@ -33,6 +25,7 @@ testX = np.expand_dims(testX, axis=-1)
 trainY = to_categorical(trainY, 10)
 testY = to_categorical(testY, 10)
 
+'''
 # initialize our optimizer and model
 print("[INFO] compiling model...")
 opt = Adam(lr=1e-3)
@@ -48,6 +41,9 @@ model.fit(trainX, trainY,
     verbose=1)
 
 model.save('simple_model.h5')
+'''
+
+model = load_model('simple_model.h5')
  
 # make predictions on the testing set for the model trained on
 # non-adversarial images
@@ -55,47 +51,30 @@ model.save('simple_model.h5')
 print("[INFO] loss: {:.4f}, acc: {:.4f}".format(loss, acc))
 
 # loop over a sample of our testing images
-for i in np.random.choice(np.arange(0, len(testX)), size=(10,)):
-    # grab the current image and label
-    image = testX[i]
-    label = testY[i]
-    # generate an image adversary for the current image and make
-    # a prediction on the adversary
-    adversary = generate_image_adversary(model,
-        image.reshape(1, 28, 28, 1), label, eps=0.1)
-    pred = model.predict(adversary)
+epsilons = [0.1]
+for eps in epsilons:
+    for i in range(len(testX)):
+        # grab the current image and label
+        image = testX[i]
+        label = testY[i]
+        # generate an image adversary for the current image and make
+        # a prediction on the adversary
+        adversary = generate_image_adversary(model,
+            image.reshape(1, 28, 28, 1), label, eps=eps)
+        pred = model.predict(adversary)
 
-    # scale both the original image and adversary to the range
-    # [0, 255] and convert them to an unsigned 8-bit integers
-    adversary = adversary.reshape((28, 28)) * 255
-    adversary = np.clip(adversary, 0, 255).astype("uint8")
-    image = image.reshape((28, 28)) * 255
-    image = image.astype("uint8")
-    # convert the image and adversarial image from grayscale to three
-    # channel (so we can draw on them)
-    image = np.dstack([image] * 3)
-    adversary = np.dstack([adversary] * 3)
-    # resize the images so we can better visualize them
-    image = cv2.resize(image, (96, 96))
-    adversary = cv2.resize(adversary, (96, 96))
-    
-    # determine the predicted label for both the original image and
-    # adversarial image
-    imagePred = label.argmax()
-    adversaryPred = pred[0].argmax()
-    color = (0, 255, 0)
-    # if the image prediction does not match the adversarial
-    # prediction then update the color
-    if imagePred != adversaryPred:
-        color = (0, 0, 255)
-    # draw the predictions on the respective output images
-    cv2.putText(image, str(imagePred), (2, 25),
-        cv2.FONT_HERSHEY_SIMPLEX, 0.95, (0, 255, 0), 2)
-    cv2.putText(adversary, str(adversaryPred), (2, 25),
-        cv2.FONT_HERSHEY_SIMPLEX, 0.95, color, 2)
-    # stack the two images horizontally and then show the original
-    # image and adversarial image
-    output = np.hstack([image, adversary])
-    cv2.imwrite('adversarial_examples/{}.jpg'.format(i), output)
-    # cv2.imshow("FGSM Adversarial Images", output)
-    # cv2.waitKey(0)
+        # scale both the original image and adversary to the range
+        # [0, 255] and convert them to an unsigned 8-bit integers
+        adversary = adversary.reshape((28, 28)) * 255
+        adversary = np.clip(adversary, 0, 255).astype("uint8")
+
+        imagePred = label.argmax()
+        adversaryPred = pred[0].argmax()
+        
+        # if imagePred == adversaryPred:
+        #     pass
+        
+        cv2.imwrite('eps_0.10/{}_{}_{}.jpg'.format(imagePred, adversaryPred,i), adversary)
+        
+        if i == 3001:
+            break
